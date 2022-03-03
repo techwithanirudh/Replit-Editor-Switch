2987f371de1fa38c3cf3010a6d84db764a8ee2b8
techwithanirudh <70373803+techwithanirudh@users.noreply.github.com>
2022-03-03 22:31:29 +0530

    Update background.js

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ async function registerScript(message) {
   registered = chrome.tabs.executeScript(null, {
     code: `
     URL =
-    "https://gist.githubusercontent.com/techwithanirudh/b6f0cec5275c8dcf75e15dc5c79a0177/raw/dbe2df52020a63a0096ed1a3fe01dc09541117ad/replitMultiEditor.js";
+    "https://gist.githubusercontent.com/techwithanirudh/b6f0cec5275c8dcf75e15dc5c79a0177/raw/replitMultiEditor.js";
   response = fetch(URL);
   response.then((response) => {
     response.text().then((script) => {
